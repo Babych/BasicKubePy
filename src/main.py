651549cbec80a6179da369c70cc3139cb8c833a9
dmytro babych <memory1024@gmail.com>
2025-12-15 16:38:29 +0200

    - code upd

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+@app.get("/temperature")
+def get_temperature(lat: float, lon: float):
+    url = (
+        "https://api.open-meteo.com/v1/forecast"
+        f"?latitude={lat}&longitude={lon}&current=temperature_2m"
+    )
+
+    response = requests.get(url, timeout=5)
+    response.raise_for_status()
+
+    data = response.json()
+    return {
+        "latitude": lat,
+        "longitude": lon,
+        "temperature": data["current"]["temperature_2m"],
+        "unit": data["current_units"]["temperature_2m"]
+    }
